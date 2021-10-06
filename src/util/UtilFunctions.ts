f9db102893b0cc91705994e4128685581d91c637
taron.geghamyan <taron.geghamyan@gmail.com>
2021-10-06 10:58:49 +0400

    add util

---
+++ src/util/UtilFunctions.ts
@@ -0,0 +1,34 @@
+export const deepClone = <T>(data: T) => {
+  return JSON.parse(JSON.stringify(data));
+};
+
+export const generateId = () => {
+  return "_" + Math.random().toString(36).substr(2, 9);
+};
+
+export const groupByDate = <T extends { created_at: string }>(
+  data: T[],
+  forSeconds: number
+) => {
+  const result: { [key: string]: T[] } = {};
+  let timeDiff = new Date(data[0].created_at).getTime() + forSeconds * 1000;
+
+  data.forEach((element) => {
+    const dateInMiliSeconds = new Date(element.created_at).getTime();
+    if (dateInMiliSeconds <= timeDiff) {
+      if (result[timeDiff]) {
+        result[timeDiff].push(element);
+      } else {
+        result[timeDiff] = [element];
+      }
+    } else {
+      timeDiff = dateInMiliSeconds + 5000;
+      result[timeDiff] = [element];
+    }
+  });
+
+  return Object.keys(result).map((date: string) => ({
+    date,
+    data: result[date],
+  }));
+};
