efbd0058a7b6ea41bb3b403e5118dd6745945006
taron.geghamyan <taron.geghamyan@gmail.com>
2021-10-06 10:10:18 +0400

    add notepad page view

--- src/pages/Notepad.tsx
+++ src/pages/Notepad.tsx
@@ -1,11 +1,126 @@
 import React from "react";
+import Button, {ButtonTypeEnum} from "../components/button";
+import Input from "../components/input";
+import Textarea from "../components/textarea";
 
 interface IProps {
     children: JSX.Element;
 }
 
 const Notepad: React.FC<IProps> = ({ children }) => {
-    return <div> {children} </div>;
+
+    const handleClick = () => {
+        alert(true)
+    };
+    const handleChange = () => {
+        console.log(true)
+    };
+
+    return (<div className="container">
+        <form className="editable-form">
+            <section className="title_of_notepad">
+                <div className="row mx-auto justify-content-between">
+                    <div className="col-6">
+                        <Input
+                            name="notepad"
+                            placeholder="notepad title"
+                            defaultValue=""
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="col-6">
+                        <div className="row mx-auto justify-content-end">
+                            <div className="col-3">
+                                <Button
+                                    text="View State"
+                                    onClick={handleClick}
+                                    b={ButtonTypeEnum.view}
+                                />
+                            </div>
+                            <div className="col-3">
+                                <Button
+                                    text="Save"
+                                    onClick={handleClick}
+                                    b={ButtonTypeEnum.save}
+                                />
+                            </div>
+                            <div className="col-3">
+                                <Button
+                                    text="Delete"
+                                    onClick={handleClick}
+                                    b={ButtonTypeEnum.delete}
+                                />
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+            <section className="add_new_note">
+                <div className="row " >
+                    <div className="col">
+                        <div className="row mx-auto justify-content-between">
+                            <div className="col-6">
+                                <Input
+                                    name="title"
+                                    placeholder="New note title"
+                                    defaultValue="New note title"
+                                    onChange={handleChange}
+                                />
+                            </div>
+                        </div>
+                        <div className="row mx-auto justify-content-between">
+                            <div className="col-6">
+                                <Textarea name="title_new_note" placeholder="New Note Text">
+                                    New Note Text
+                                </Textarea>
+                            </div>
+                        </div>
+                        <div className="row mx-auto justify-content-between">
+                            <div className="col-6">
+                                <Button
+                                    text="Add"
+                                    onClick={handleClick}
+                                    b={ButtonTypeEnum.add}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="list_of_notes">
+                <div className="row " >
+                    <div className="col">
+                        <div className="row mx-auto justify-content-between">
+                            <div className="col-6">
+                                <Input
+                                    name="title"
+                                    placeholder="New note title"
+                                    defaultValue="New note title"
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div className="col-6">
+                                <Button
+                                    text="Delete"
+                                    onClick={handleClick}
+                                    b={ButtonTypeEnum.delete}
+                                />
+                            </div>
+                        </div>
+                        <div className="row mx-auto justify-content-between">
+                            <div className="col-6">
+                                <Textarea name="title_new_note" placeholder="New Note Text">
+                                    New Note Text
+                                </Textarea>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </form>
+        {children}
+    </div>);
 };
 
 export default Notepad;
