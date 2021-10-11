42f224a67276568160b1ea5297980d3c01214c37
taron.geghamyan <taron.geghamyan@gmail.com>
2021-10-11 11:56:17 +0400

    code review for constants

--- src/config/api/constants.ts
+++ src/config/api/constants.ts
@@ -1,10 +1,5 @@
 /**
  *  App configs
  */
-
-export const CREATE_ROUTE_FOR_NOTEPAD: (id: string) => string = (id: string) =>
-  `/notepads/${id}`;
-export const AUTHORIZE_TOKEN: string | undefined =
-  process.env.REACT_APP_GITHUB_AUTHORIZE_TOKEN;
-export const GITHUB_API_URL: string | undefined =
-  process.env.REACT_APP_GITHUB_API_URL;
+export const AUTHORIZE_TOKEN: string | undefined = process.env.REACT_APP_GITHUB_AUTHORIZE_TOKEN;
+export const GITHUB_API_URL: string | undefined = process.env.REACT_APP_GITHUB_API_URL;
